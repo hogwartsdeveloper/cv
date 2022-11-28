4924968b8e8a522f57ded3451e7b37d9beccc778
zhannur <jannuraidynuly@gmail.com>
2022-11-28 14:29:16 +0600

    logo decomposition

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { TranslateModule } from '@ngx-translate/core';
 import { BtnComponent } from '../ui/btn/btn.component';
+import { LogoComponent } from '../ui/logo/logo.component';
 
 @Component({
   standalone: true,
-  imports: [MatButtonModule, TranslateModule, BtnComponent],
+  imports: [MatButtonModule, TranslateModule, BtnComponent, LogoComponent],
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills.component';
 import { SectionTitleComponent } from '../ui/section-title/section-title.component';
+import { LogoComponent } from '../ui/logo/logo.component';
 
 @NgModule({
   declarations: [SkillsComponent],
-  imports: [CommonModule, SectionTitleComponent],
+  imports: [CommonModule, SectionTitleComponent, LogoComponent],
   exports: [SkillsComponent],
 })
 export class SkillsModule {}
