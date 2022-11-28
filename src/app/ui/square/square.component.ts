7794e41da9bb212f3f341fa492d4b39d84c15abc
zhannur <jannuraidynuly@gmail.com>
2022-11-28 23:25:32 +0600

    add width and height settings

--- src/app/ui/square/square.component.ts
+++ src/app/ui/square/square.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-square',
   standalone: true,
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.scss'],
+  imports: [NgClass],
 })
 export class SquareComponent implements OnInit {
+  @Input() width: number = 20;
+  @Input() height: number = 20;
   constructor() {}
 
   ngOnInit(): void {}
