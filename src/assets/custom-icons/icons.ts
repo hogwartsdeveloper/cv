3f367f65edc9b5b02b83e22a8a757cb9c763c947
zhannur <jannuraidynuly@gmail.com>
2022-12-01 22:30:10 +0600

    footer layout

--- src/assets/custom-icons/icons.ts
+++ src/assets/custom-icons/icons.ts
@@ -1,21 +1,29 @@
 export type IconsId =
   | 'email'
   | 'github'
-  | 'linkedin';
+  | 'linkedin'
+  | 'menu-close'
+  | 'menu';
 
 export type IconsKey =
   | 'Email'
   | 'Github'
-  | 'Linkedin';
+  | 'Linkedin'
+  | 'MenuClose'
+  | 'Menu';
 
 export enum Icons {
   Email = 'email',
   Github = 'github',
   Linkedin = 'linkedin',
+  MenuClose = 'menu-close',
+  Menu = 'menu',
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
   [Icons.Email]: '61697',
   [Icons.Github]: '61698',
   [Icons.Linkedin]: '61699',
+  [Icons.MenuClose]: '61700',
+  [Icons.Menu]: '61701',
 };
