03e8731b1bd9c158743aefb2f9d22a77e6ca44f7
zhannur <jannuraidynuly@gmail.com>
2023-05-28 13:20:56 +0600

    add local translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
 import { ToolbarModule } from './toolbar/toolbar.module';
-import {
-  HTTP_INTERCEPTORS,
-  HttpClient,
-  HttpClientModule,
-} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ProjectsModule } from './projects/projects.module';
@@ -18,7 +14,6 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoaderComponent } from './ui/loader/loader.component';
-import { HttpRequestInterceptor } from './common/http-request.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -42,17 +37,10 @@ import { HttpRequestInterceptor } from './common/http-request.interceptor';
     FooterComponent,
     LoaderComponent,
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpRequestInterceptor,
-      multi: true,
-    },
-  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, '/api/local/', '');
+  return new TranslateHttpLoader(http);
 }
